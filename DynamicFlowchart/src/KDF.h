f4d29df4b95e0dd8f91013eeb6ceb355a669b65b
Kuenlun <kuenlun13054@gmail.com>
2021-11-07 00:00:30 +0100

    Adding spdlog library

--- DynamicFlowchart/src/KDF.h
+++ DynamicFlowchart/src/KDF.h
@@ -2,4 +2,7 @@
 
 // For use by KDF applications
 #include "KDF/Application.h"
+#include "KDF/Log.h"
+
+// Entry Point
 #include "KDF/EntryPoint.h"
--- DynamicFlowchart/src/KDF/EntryPoint.h
+++ DynamicFlowchart/src/KDF/EntryPoint.h
@@ -6,6 +6,10 @@ extern KDF::Application* KDF::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	KDF::Log::Init();
+	KDF::Log::GetCoreLogger()->warn("Initialized Log");
+	KDF::Log::GetClientLogger()->info("Initialized Log");
+
 	auto app = KDF::CreateApplication();
 	app->Run();
 	delete app;
